c0d1763c105f10bff1de7ace5fcf79da932cf9e0
Brian Jackson <bjack205@gmail.com>
2021-09-08 10:37:14 -0400

    Fix pub tests

--- test/publisher_tests.jl
+++ test/publisher_tests.jl
@@ -2,11 +2,12 @@ import Mercury as Hg
 using ZMQ
 using Sockets
 using Test
+using BenchmarkTools
 include("jlout/test_msg_pb.jl")
 
 # Construction
 Hg.reset_pub_count()
-ctx = ZMQ.Context(1)
+ctx = ZMQ.Context()
 addr = ip"127.0.0.1"
 port = 5555
 pub = Hg.Publisher(ctx, addr, port)
@@ -18,17 +19,17 @@ pub.name == "publisher_1"
 isopen(pub)
 
 @test_throws ZMQ.StateError Hg.Publisher(ctx, addr, port)
-isopen(pub)
+@test isopen(pub)
 close(pub)
-!isopen(pub.socket)
+@test !isopen(pub.socket)
 
 pub2 = Hg.Publisher(ctx, addr, string(port))
-pub2.name == "publisher_3"
+@test pub2.name == "publisher_3"
 close(pub2)
 
 pub3 = Hg.Publisher(ctx, string(addr), port, name="mypub")
-pub3.name == "mypub"
-pub3.port == port
+@test pub3.name == "mypub"
+@test pub3.port == port
 
 # Make sure the garbage collector never runs when publishing
 msg = TestMsg(x=10, y=11, z=12)
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,3 +6,5 @@ outdir = joinpath(@__DIR__, "jlout")
 protodir = joinpath(@__DIR__, "proto")
 msgfile = joinpath(protodir, "test_msg.proto")
 ProtoBuf.protoc(`-I=$protodir --julia_out=$outdir $msgfile`)
+
+include("publisher_tests.jl")
