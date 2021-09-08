517e5e7fa83e8599ecf0ccfa05b68a4094f95c05
Brian Jackson <bjack205@gmail.com>
2021-09-08 10:40:20 -0400

    Make pub test suite

--- test/publisher_tests.jl
+++ test/publisher_tests.jl
@@ -6,32 +6,41 @@ using BenchmarkTools
 include("jlout/test_msg_pb.jl")
 
 # Construction
-Hg.reset_pub_count()
-ctx = ZMQ.Context()
-addr = ip"127.0.0.1"
-port = 5555
-pub = Hg.Publisher(ctx, addr, port)
+@testset "Publisher" begin
+    Hg.reset_pub_count()
+    ctx = ZMQ.Context()
+    addr = ip"127.0.0.1"
+    port = 5555
 
-pub.port == port
-pub.ipaddr == addr
-Hg.tcpstring(pub) == "tcp://$addr:$port"
-pub.name == "publisher_1"
-isopen(pub)
+    @testset "Pub Construction" begin
+        pub = Hg.Publisher(ctx, addr, port)
 
-@test_throws ZMQ.StateError Hg.Publisher(ctx, addr, port)
-@test isopen(pub)
-close(pub)
-@test !isopen(pub.socket)
+        pub.port == port
+        pub.ipaddr == addr
+        Hg.tcpstring(pub) == "tcp://$addr:$port"
+        pub.name == "publisher_1"
+        isopen(pub)
 
-pub2 = Hg.Publisher(ctx, addr, string(port))
-@test pub2.name == "publisher_3"
-close(pub2)
+        @test_throws ZMQ.StateError Hg.Publisher(ctx, addr, port)
+        @test isopen(pub)
+        close(pub)
+        @test !isopen(pub.socket)
 
-pub3 = Hg.Publisher(ctx, string(addr), port, name="mypub")
-@test pub3.name == "mypub"
-@test pub3.port == port
+        pub2 = Hg.Publisher(ctx, addr, string(port))
+        @test pub2.name == "publisher_3"
+        close(pub2)
 
-# Make sure the garbage collector never runs when publishing
-msg = TestMsg(x=10, y=11, z=12)
-b = @benchmark Hg.publish($pub3, $msg)
-@test maximum(b.gctimes) == 0.0
+        pub3 = Hg.Publisher(ctx, string(addr), port, name = "mypub")
+        @test pub3.name == "mypub"
+        @test pub3.port == port
+        close(pub3)
+    end
+
+    # Make sure the garbage collector never runs when publishing
+    @testset "Pub performance" begin
+        pub = Hg.Publisher(ctx, addr, port)
+        msg = TestMsg(x = 10, y = 11, z = 12)
+        b = @benchmark Hg.publish($pub, $msg)
+        @test maximum(b.gctimes) == 0.0
+    end
+end
