34f47b3fdea803dc579e556927c8cd9ecf921631
Alexander A Bouman <alex.bouman@gmail.com>
2021-09-14 13:16:30 -0400

    Changed library name depending on OS

--- src/Mercury.jl
+++ src/Mercury.jl
@@ -4,7 +4,14 @@ import Sockets
 import ProtoBuf
 import Logging
 
-const libhg = joinpath(@__DIR__, "..", "deps", "build", "libhg.so")
+# Import correct library name for specific system
+libhg_library_filename = ""
+if Sys.islinux()
+    libhg_library_filename = joinpath(@__DIR__, "..", "deps", "build", "libhg.so")
+elseif Sys.isapple()
+    libhg_library_filename = joinpath(@__DIR__, "..", "deps", "build", "libhg.dylib")
+end
+const libhg = libhg_library_filename
 
 
 greet() = print("Hello World!")
