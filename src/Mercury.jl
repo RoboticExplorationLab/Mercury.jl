37e01c06d1f3c7df8bc94ea3d66c8c050cb963d7
Alexander A Bouman <alex.bouman@gmail.com>
2021-09-17 16:58:42 -0400

    Fixed conflate issues

--- src/Mercury.jl
+++ src/Mercury.jl
@@ -4,8 +4,37 @@ import Sockets
 import ProtoBuf
 import Logging
 
-const libhg = joinpath(@__DIR__, "..", "deps", "build", "libhg.so")
+# Import correct library name for specific system
+libhg_library_filename = ""
+if Sys.islinux()
+    libhg_library_filename = joinpath(@__DIR__, "..", "deps", "build", "libhg.so")
+elseif Sys.isapple()
+    libhg_library_filename = joinpath(@__DIR__, "..", "deps", "build", "libhg.dylib")
+end
+const libhg = libhg_library_filename
 
+
+ZMQ_CONFLATE = 54
+
+# Set conflate option for ZMQ, not included in ZMQ.jl
+function set_conflate(socket::ZMQ.Socket, option_val::Integer)
+    rc = ccall((:zmq_setsockopt, ZMQ.libzmq), Cint,
+            (Ptr{Cvoid}, Cint, Ref{Cint}, Csize_t),
+            socket, ZMQ_CONFLATE, option_val, sizeof(Cint))
+    if rc != 0
+        throw(ZMQ.StateError(ZMQ.jl_zmq_error_str()))
+    end
+end
+
+# # Get the conflate option for ZMQ, not included in ZMQ.jl
+function get_conflate(socket::ZMQ.Socket, option_val::Integer)
+    rc = ccall((:zmq_getsockopt, ZMQ.libzmq), Cint,
+            (Ptr{Cvoid}, Cint, Ref{Cint}, Csize_t),
+            socket, ZMQ_CONFLATE, option_val, sizeof(Cint))
+    if rc != 0
+        throw(ZMQ.StateError(ZMQ.jl_zmq_error_str()))
+    end
+end
 
 greet() = print("Hello World!")
 include("utils.jl")
--- test/scratch.jl
+++ test/scratch.jl
@@ -1,17 +1,67 @@
 using ZMQ
 
-ctx = ZMQ.Context(1)
-sock = ZMQ.Socket(ctx, ZMQ.PUB)
-try
-    ZMQ.bind(sock, "tcp://127.0.0.1:5555")
-catch e
-    if e isa ZMQ.StateError
-        @show ZMQ.zmq_errno()
-        @show ZMQ.jl_zmq_error_str()
+
+# Set conflate option for ZMQ, not included in ZMQ.jl
+# function set_conflate(socket::ZMQ.Socket, option_val::Integer)
+#     rc = ccall((:zmq_setsockopt, ZMQ.libzmq), Cint,
+#             (Ptr{Cvoid}, Cint, Ref{Cint}, Ref{Csize_t}),
+#             socket, 54, option_val, sizeof(Cint))
+#     if rc != 0
+#         println(Base.Libc.errno())
+#         throw(ZMQ.StateError(ZMQ.jl_zmq_error_str()))
+#     end
+# end
+
+# Set conflate option for ZMQ, not included in ZMQ.jl
+function get_conflate(socket::ZMQ.Socket, option_val::Integer)
+    rc = ccall((:zmq_getsockopt, ZMQ.libzmq), Cint,
+            (Ptr{Cvoid}, Cint, Ref{Cint}, Ref{Csize_t}),
+            socket, 54, option_val, sizeof(Cint))
+    if rc != 0
+        println(Base.Libc.errno())
+        throw(ZMQ.StateError(ZMQ.jl_zmq_error_str()))
     end
 end
 
-@which ZMQ.bind(sock, "tcp://127.0.0.1:5555")
+# Set conflate option for ZMQ, not included in ZMQ.jl
+function set_conflate(socket::ZMQ.Socket, option_val::Integer)
+    Cint_size = sizeof(Cint)
 
+    rc = ccall((:zmq_setsockopt, ZMQ.libzmq), Cint,
+            (Ptr{Cvoid}, Cint, Ref{Cint}, Csize_t),
+            socket, 54, option_val, Cint_size)
+    if rc != 0
+        println(Base.Libc.errno())
+        throw(ZMQ.StateError(ZMQ.jl_zmq_error_str()))
+    end
+end
+
+
+# %%
+ctx = ZMQ.Context(1)
+sock = ZMQ.Socket(ctx, ZMQ.SUB)
+ZMQ.subscribe(sock)
+sock
+
+# %%
+rc = set_conflate(sock, 1)
+
+# %%
+get_conflate(sock, true)
+
+# %%
+close(ctx)
+
+# %%
+close(sock)
+
+
+# set_conflate(sock, 1)
+
+
+
+# %%
 isopen(sock)
 close(sock)
+
+
